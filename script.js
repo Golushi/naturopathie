77fdd5975a1e873015ded3b755745eba66552192
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-06-13 14:06:30 +0200

    preparation structure

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const menuContainer = document.querySelector(".menu-container");
+const navLinks = document.querySelectorAll(".menu li a");
+const checkBox = document.querySelector("#check");
+
+checkBox.addEventListener("change", function () {
+  menuContainer.classList.toggle("show");
+  hamburger.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+      menuContainer.classList.remove("show");
+      hamburger.classList.remove("active");
+      checkBox.checked = false;
+    }
+  });
+});
